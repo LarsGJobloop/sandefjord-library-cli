57314d9cc671c28eeca1df4cdaa44aa7a5ba3daa
larsg <lars.gunnar@jobloop.no>
2024-11-12 11:52:54 +0100

    setup user io handling

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
-﻿bool runProgram = true;
+﻿// Sett opp biblioteket
+Library library = new Library();
+
+// Lag bøkene
+Book martian = new Book("Martian", "Jane Doe", new DateTime(2004, 3, 25));
+
+// Legg til bøker i biblioteket
+library.AddNewBook(martian);
+
+// Håndter bruker input
+bool runProgram = true;
 while (runProgram)
 {
   // les av bruker input
@@ -6,10 +16,40 @@ while (runProgram)
   string? userInput = Console.ReadLine();
 
   // Vi må finne hva bruker skrev inn
-  // For å låne en bok (lend)
-  if (userInput == "lend")
+
+  // List ut tilgjengelige bøker
+  if (userInput == "list")
   {
-    Console.WriteLine("Lending a book");
+    Console.WriteLine("Here are available books:");
+    List<Book> availableBooks = library.ListAvailableBooks();
+
+    foreach (var book in availableBooks)
+    {
+      Console.WriteLine(book.Title);
+    }
+  }
+  // For å låne en bok (lend)
+  else if (userInput == "lend")
+  {
+    Console.WriteLine("What is the title of the book?");
+    string? wantedBookTitle = Console.ReadLine();
+
+    if (wantedBookTitle == null)
+    {
+      continue; // Start hoved løkken på nytt
+    }
+
+    Book? book = library.LendBook(wantedBookTitle);
+
+    // Det kan hend biblioteket ikke hadde boken vår
+    if (book == null)
+    {
+      Console.WriteLine("No book with title found: " + wantedBookTitle);
+    }
+    else
+    {
+      Console.WriteLine("Lending book: " + book.Title);
+    }
   }
   // For å lever tilbake en bok (return)
   else if (userInput == "return")

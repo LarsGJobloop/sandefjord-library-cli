4e5e6e402b4c59be00ead999c09c4e35e98502a3
larsg <lars.gunnar@jobloop.no>
2024-11-12 10:31:24 +0100

    setup user input parsing

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
-﻿// les av bruker input
-Console.WriteLine("Write something in the console:");
-string? userInput = Console.ReadLine();
+﻿bool runProgram = true;
+while (runProgram)
+{
+  // les av bruker input
+  Console.WriteLine("Do you want to lend or return?");
+  string? userInput = Console.ReadLine();
 
-// Ekko tilbake det bruker skrev inn
-Console.WriteLine("Echo: " + userInput);
+  // Vi må finne hva bruker skrev inn
+  // For å låne en bok (lend)
+  if (userInput == "lend")
+  {
+    Console.WriteLine("Lending a book");
+  }
+  // For å lever tilbake en bok (return)
+  else if (userInput == "return")
+  {
+    Console.WriteLine("Returning a book");
+  }
+  // For å avslutte (exit)
+  else if (userInput == "exit")
+  {
+    runProgram = false;
+  }
+}
